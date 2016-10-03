ff06f561d4ec8da4fe52326bff9c56d479dfe3a4
tribbyx <christoffer.ssj@gmail.com>
2016-10-03 10:23:10 +0100

    Added a DBHelper and refactored the code in ServiceStudentCrud.svc to fit the new DBHelper.
    Also the database has been added to Azure

--- SoapStudentCrudFuck/SoapStudentCrudFuck/ServiceStudentCrud.svc.cs
+++ SoapStudentCrudFuck/SoapStudentCrudFuck/ServiceStudentCrud.svc.cs
@@ -15,7 +15,7 @@ namespace SoapStudentCrudFuck
     public class Service1 : IService1
     {
         string connectionString =
-                @"Data Source=(localdb)\ProjectsV13;Initial Catalog=CloudStudent;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                @"Data Source=students-server.database.windows.net;Initial Catalog=Students;Integrated Security=False;User ID=chri56a4;Password=K03g3bugt;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         private static List<Student> _students = new List<Student>();
 
@@ -24,28 +24,7 @@ namespace SoapStudentCrudFuck
         /// </summary>
         public List<Student> GetAllStudents()
         {
-            string queryString = $"SELECT * FROM [Table]";
-            var studentList = new List<Student>();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                SqlCommand cmd = new SqlCommand(queryString, connection);
-
-                var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    var s = new Student();
-                    s.FirstName = reader.GetString(1);
-                    s.LastName = reader.GetString(2);
-                    s.Cpr = reader.GetString(3);
-                    s.Age = reader.GetInt32(4);
-                    studentList.Add(s);
-                }
-                connection.Close();
-                return studentList;
-            }
-            return null;
+            return DbHelper.GetAllStudents();
         }
 
         /// <summary>
@@ -56,26 +35,7 @@ namespace SoapStudentCrudFuck
         /// <returns></returns>
         public Student GetStudent(Student student)
         {
-            string queryString = $"SELECT * FROM [Table] WHERE cpr = '{student.Cpr}'";
-            var returnStudent = new Student();
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                SqlCommand cmd = new SqlCommand(queryString, connection);
-
-                var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    returnStudent.FirstName = reader.GetString(1);
-                    returnStudent.LastName = reader.GetString(2);
-                    returnStudent.Cpr = reader.GetString(3);
-                    returnStudent.Age = reader.GetInt32(4);
-                }
-                connection.Close();
-                return returnStudent;
-            }
-            return null;
+            return DbHelper.GetStudent(student);
         }
 
         /// <summary>
@@ -85,9 +45,7 @@ namespace SoapStudentCrudFuck
         /// <param name="student"></param>
         public bool AddStudents(Student student)
         {
-            string queryString =
-                $"INSERT INTO [Table] values ('{student.FirstName}', '{student.LastName}','{student.Cpr}','{student.Age}')";
-            if (DBCommand(queryString)) return true;
+            if (DbHelper.AddStudents(student)) return true;
             return false;
         }
         /// <summary>
@@ -97,10 +55,7 @@ namespace SoapStudentCrudFuck
         /// <param name="student"></param>
         public bool DeleteStudent(Student student)
         {
-            string queryString =
-               $"DELETE * FROM [Table] WHERE cpr='{student.Cpr}'";
-
-            if (DBCommand(queryString)) return true;
+            if (DbHelper.DeleteStudent(student)) return true;
             return false;
         }
         /// <summary>
@@ -111,8 +66,7 @@ namespace SoapStudentCrudFuck
         /// <param name="student"></param>
         public bool UpdateStudent(int index, Student student)
         {
-            string queryString = $"UPDATE * [Table] SET firstName='{student.FirstName}', lastName='{student.LastName}', cpr='{student.Cpr}', age='{student.Age}'";
-            if (DBCommand(queryString)) return true;
+            if (DbHelper.UpdateStudent(index, student)) return true;
             return false;
         }
 
@@ -124,50 +78,7 @@ namespace SoapStudentCrudFuck
         /// <returns></returns>
         public List<Student> GetStudentByFirstName(string firstName)
         {
-            string queryString = $"SELECT * FROM [Table] WHERE firstName = '{firstName}'";
-            List<Student> tobeReturnedList = new List<Student>();
-
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                SqlCommand cmd = new SqlCommand(queryString, connection);
-                var reader = cmd.ExecuteReader();
-                while (reader.Read())
-                {
-                    Student s = new Student();
-                    s.FirstName = reader.GetString(1);
-                    s.LastName = reader.GetString(2);
-                    s.Cpr = reader.GetString(3);
-                    s.Age = reader.GetInt32(4);
-                    tobeReturnedList.Add(s);
-                }
-                return tobeReturnedList;
-            }
-        }
-
-        /// <summary>
-        /// Connects to the database, and gives it the query string.
-        /// takes a string containing an SQL query as parameter.  
-        /// </summary>
-        /// <param name="queryString"></param>
-        /// <returns></returns>
-        public bool DBCommand(string queryString)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                var cmd = new SqlCommand();
-
-                cmd.CommandText = queryString;
-                cmd.CommandType = CommandType.Text;
-                cmd.Connection = connection;
-
-                connection.Open();
-                cmd.ExecuteReader();
-
-                connection.Close();
-                return true;
-            }
+            return DbHelper.GetStudentByFirstName(firstName);
         }
     }
 }
